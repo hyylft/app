9f40ade9807b7301813eed58b6062426841ed458
hyy <rdsht520@163.com>
2016-07-15 21:46:32 +0800

    edited

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var views = '../views/';
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render(views +'myIndex.html');
+  res.render(views+'myIndex.html',routes);
 });
 
 module.exports = router;
